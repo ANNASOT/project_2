95a5cb92c569ba82f2a8007f05a8ab0ae6865c41
unknown <annasotnika@inbox.lv>
2019-12-30 14:30:59 +0200

    FirstCommit

---
+++ script.js
@@ -0,0 +1,32 @@
+~'use strict';
+
+let money = prompt("Ваш бюджет на месяц?", "");
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+console.log(money, time);
+
+let appData = {
+    budget : money,
+    timeData : time,
+    expenses : {},
+    optionalExpensesData : {},
+    incomeData : [],
+    savings : false,
+};
+
+console.log(appData);
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    a2 = prompt("Во сколько обойдется?"),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    a4 = prompt("Во сколько обойдется?");
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
+console.log(alert);
+
+
+
+
